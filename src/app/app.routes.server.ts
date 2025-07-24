55df3c23e91f85c8be4adb03b5d5b8d3a1637395
SheeYeapChuah <schuah@wcc-group.com>
2025-07-24 16:32:08 +0800

    Lab 4: add comment

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,8 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 // Define the strategy for rendering
 // ** -> by default all is Pre-render
-// options: Server (SSR), Pre-render (SSG), Client (CSR)
+// options: Server (SSR), Pre-render (SSG - static-site generation), Client (CSR)
+// Rendering speed: SSG > SSR > CSR (may display a white page and then static content before being fully rendered with the data on request call on client side)
 // For SSR, run with the command: npm run serve:ssr:ssr-lab
 export const serverRoutes: ServerRoute[] = [
 	{
